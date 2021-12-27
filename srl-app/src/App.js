26fc8b3e213bc65841fbca2961fd5919c37cf87b
Sulea Serban <suleaserbans@gmail.com>
2021-12-28 00:27:37 +0200

    Navbar cu orar

--- srl-app/src/App.js
+++ srl-app/src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
+import logo from './logo_sighy.jpg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import { Navbar } from 'react-bootstrap'
+import { Nav, Navbar } from 'react-bootstrap'
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="myRed" variant="dark">
         <Navbar.Brand>
+          <img src={logo} witdh="40px" height="40px" />{' '}
           Sighy Gym
         </Navbar.Brand>
+        <Nav>
+          <Nav.Link href="Orar">Orar</Nav.Link>
+          </Nav>
 
         </Navbar>
     
